e9ec07f82d450637260c29c9dd366775a597e96d
rizky <rja@its.ac.id>
2025-09-15 10:01:29 +0700

    ada sample project

---
+++ src/SampleApp/SampleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c = new Calculator();
+            var result = c.Add(1, 2);
+
+            Console.WriteLine(result);
+        }
+    }
+}
